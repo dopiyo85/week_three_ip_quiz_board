bc1e785fb9cc29bf7655b88cf89954639e56b1fc
Diana Opiyo <dopiyo@gmail.com>
2022-11-04 13:06:13 +0300

    worked on the js file

--- js/quizboard.js
+++ js/quizboard.js
@@ -1,27 +1,46 @@
-const quizAnswers = document.querySelectorAll(".ouranswers");
-const submitAnswer = document.querySelector("#submit_ans");
-let marks = 0;
-let total = 0;
-quizAnswers.forEach(function (jibu) {
-    if(parseInt(jibu.value) > 0) {
-        total += parseInt(jibu.value);
-    }
-    else{
-        total = total;
-    }
-    jibu.addEventListener("change", function (event) {
-        marks += parseInt(event.target.value);
+$(document).ready(function () {
+    const quizAnswers = document.querySelectorAll(".ouranswers");
+    const submitAnswer = document.querySelector("#submit_ans");
+    let marks = 0;
+    let total = 0;
+    quizAnswers.forEach(function (jibu) {
+        if (parseInt(jibu.value) > 0) {
+            total += parseInt(jibu.value);
+        }
+        else {
+            total = total;
+        }
+        jibu.addEventListener("change", function (event) {
+            marks += parseInt(event.target.value);
+        });
+    });
+    submitAnswer.addEventListener("click", function (event) {
+        let score = (marks / total) * 100;
+        //     if (event.target.submitAnswer == score &&
+        //     !event.target.hasAttribute("target"))
+        // {
+        // event.target.setAttribute("target", "_blank");
+        // }
+        alert("your score is " + score + "%");
+    });
+    $("#submit_ans").click(function () {
+        alert("Wait for your Score");
+    });
+    $("#jumboimage").hover(function () {
+        $(this).mouseenter(function () {
+            $("#jumboimage").hide()
+        })
+        $(this).mouseleave(function () {
+            $("#jumboimage").show();
+        })
+
     });
 });
-submitAnswer.addEventListener("click", function (event) {
-    let score = (marks/total)*100;
-    if (event.target.submitAnswer == score &&
-    !event.target.hasAttribute("target"))
-{
-event.target.setAttribute("target", "_blank");
-}
-    //   alert("your score is " + score + "%");   
-}); 
+
+
+// $(window).reload(function(event){
+//     $("body").css("background-image","images/background2.jpg")
+// });
 
 // function grader(){
 //     var grades = score;
@@ -29,16 +48,16 @@ event.target.setAttribute("target", "_blank");
 //           alert("Your grade is A");
 //           }else if(grades >=60 && grades <=79){
 //           alert("Your grade is B");
-//           } 
+//           }
 //            else if(grades >=40 && grades <=59){
 //           alert("Your grade is C");
 //           }
 //   else if(grades >=20 && grades <=39){
 //           alert("Your grade is D");
-//           } 
+//           }
 //           else if(grades >=0 && grades <=20){
 //           alert("Your grade is E");
-//           }  
-// }   
+//           }
+// }
 // grader();
-      
+
